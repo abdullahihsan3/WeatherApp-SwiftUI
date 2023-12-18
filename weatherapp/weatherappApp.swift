6bb22e0ba4a23673b919e818008af3978d7b851d
Abdullah Ihsan <abdullah.ihsan3@gmail.com>
2023-12-18 13:30:34 +0500

    Initial Commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Dev on 18/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
